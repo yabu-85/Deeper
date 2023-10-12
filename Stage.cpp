b368e2e11383f466313bc5eb0f2713c358feb0bc
海斗 矢吹 <yabu-85@outlook.jp>
2023-10-12 18:24:14 +0900

    Ray分割未完成

--- Stage.cpp
+++ Stage.cpp
@@ -4,15 +4,14 @@
 
 //Rayの分割用のてすとようでーた
 #include "Player.h"
+#include "Engine/Text.h"
 
 namespace {
     const float playerRadius = 1.0f;
     const float boxSize = 10.0f;
     const int maxCount = 8;
-    
-    //多分８個以上はつかわないかな
-    int boxCount[maxCount] = { 0,0,0,0,0,0,0,0 };
 
+    Text *pText = new Text;
 
 }
 
@@ -40,6 +39,8 @@ void Stage::Initialize()
     intersectDatas_.push_back( { hModel_[MAX + RT2], XMFLOAT3(10.0f, 0.0f, 0.0f)} );
     intersectDatas_.push_back( { hModel_[MAX + RT2], XMFLOAT3(-10.0f, 0.0f, 0.0f)} );
 
+    pText->Initialize();
+
 }
 
 void Stage::Update()
@@ -47,25 +48,28 @@ void Stage::Update()
     //なんかここで四角の中のポリゴン取得したりしたい
     if (!Input::IsKeyDown(DIK_SPACE)) return;
 
-
     Player* pPlayer = (Player*)FindObject("Player");
     XMFLOAT3 plaPos = pPlayer->GetPosition();
 
-    for (int i = 0; i < maxCount; i++) {
-
-    }
-
-
-
-    for (int i = 0; i < intersectDatas_.size(); i++) {
-        Model::GetFbx(intersectDatas_[i].hModelNum);
-    }
-
     //プレイヤーの位置を取得して、判定距離内に入った分割ブロックを取得
+    float fBox[3] = { plaPos.x / boxSize, plaPos.y / boxSize, plaPos.z / boxSize };
+    int iBox[3] = { fBox[0], fBox[1], fBox[2] };
+    for (int i = 0; i < 3; i++) if (fBox[i] < 0) iBox[i] -= 1;
+    for(int i = 0;i < 3;i++) iBox[i] *= 10;
+
+    std::string strNumber[3];
+    for(int i = 0;i < 3;i++) strNumber[i] = std::to_string(iBox[i]);
+
+    OutputDebugStringA(strNumber[0].c_str());
+    OutputDebugString(", ");
+    OutputDebugStringA(strNumber[1].c_str());
+    OutputDebugString(", ");
+    OutputDebugStringA(strNumber[2].c_str());
+    OutputDebugString("\n");
+
+    //Model::GetFbx(intersectDatas_[i].hModelNum);
 
     //とったFbxデータの指定ポリゴンだけにRayCastする関数とか作って判定をしたい
-
-
 
 }
 
@@ -89,6 +93,13 @@ void Stage::Draw()
             Model::Draw(handle, 2);
         }
     }
+
+    Player* pPlayer = (Player*)FindObject("Player");
+    XMFLOAT3 plaPos = pPlayer->GetPosition();
+
+    pText->Draw(30, 30, plaPos.x);
+    pText->Draw(30, 70, plaPos.y);
+    pText->Draw(30, 110, plaPos.z);
 
 }
 

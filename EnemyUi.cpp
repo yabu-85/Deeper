d80eddf11a2c7c4af620b42cc9bd8c2ea48b6323
海斗 矢吹 <220177@jc-21.jp>
2023-10-27 13:45:44 +0900

    メモリリークな汚染

--- EnemyUi.cpp
+++ EnemyUi.cpp
@@ -18,13 +18,14 @@ EnemyUi::EnemyUi(EnemyBase* parent)
 
 EnemyUi::~EnemyUi()
 {
-    VFX::End(vHandle_[0]);
-    VFX::End(vHandle_[1]);
+//    VFX::End(vHandle_[0]);
 
 }
 
 void EnemyUi::Initialize()
 {
+    size = XMFLOAT2(maxSizeX, 0.5f);
+    
     data_.textureFileName = "Png/Gauge2.png";
     data_.delay = maxTime;
     data_.number = 1;
@@ -39,25 +40,7 @@ void EnemyUi::Initialize()
     data_.color = XMFLOAT4(1.0f, 1.0f, 0.1f, 1.0f);
     data_.deltaColor = XMFLOAT4(0.0f, 0.0f , 0.0f, 0.0f);
     data_.isBillBoard = true;
-	//vHandle_[0] = VFX::Start(data_);	//エミッターを設置
-
-    data_.textureFileName = "Png/GaugeFrame2.png";
-    data_.delay = maxTime;
-    data_.number = 1;
-    data_.lifeTime = maxTime;
-    data_.direction = XMFLOAT3(0.0f, 0.0f, 0.0f);
-    data_.directionRnd = XMFLOAT3(0.0f, 0.0f, 0.0f);
-    data_.speed = 0.0f;
-    data_.speedRnd = 0.0f;
-    data_.size = XMFLOAT2(maxSizeX, 0.5f);
-    data_.sizeRnd = XMFLOAT2(0.0f, 0.0f);
-    data_.scale = XMFLOAT2(1.0f, 1.0f);
-    data_.color = XMFLOAT4(1.0f, 1.0f, 0.1f, 1.0f);
-    data_.deltaColor = XMFLOAT4(0.0f, 0.0f, 0.0f, 0.0f);
-    data_.isBillBoard = true;
-  //  vHandle_[1] = VFX::Start(data_);	//エミッターを設置
-
-    size = XMFLOAT2(maxSizeX, 0.5f);
+//	vHandle_[0] = VFX::Start(data_);	//エミッターを設置
 
 }
 
@@ -65,15 +48,9 @@ void EnemyUi::Update()
 {
     XMFLOAT3 pos = pParent_->GetPosition();
     pos.y += 3.0f;
+    size.x = maxSizeX * parcent;
     
-    //VFX::SetParticlePosition(vHandle_[1], pos);
-
-    if (true) {
-        size.x = maxSizeX * parcent;
-        VFX::SetParticleSize(vHandle_[0], size);
-
-    }
-
-    VFX::SetParticlePosition(vHandle_[0], pos);
+//    VFX::SetParticleSize(vHandle_[0], size);
+//    VFX::SetParticlePosition(vHandle_[0], pos);
 
 }

821f1f989b0564312343b687a6fb566f8086c503
海斗 矢吹 <220177@jc-21.jp>
2024-02-09 11:36:50 +0900

    Feetをちょっといじった、ThrowEnemyをとりま追加

--- Enemy/ThrowEnemy.cpp
+++ Enemy/ThrowEnemy.cpp
@@ -1,3 +1,4 @@
+#include "ThrowEnemy.h"
 #include "Feet.h"
 #include "../Engine/Model.h"
 #include "EnemyUi.h"
@@ -13,18 +14,19 @@
 #include "../Action/RotateAction.h"
 #include "../Action/SearchAction.h"
 
-Feet::Feet(GameObject* parent)
-	: EnemyBase(parent, "FeetEnemy"), hModel_(-1), pHandCollider_(nullptr), pMoveAction_(nullptr), pRotateAction_(nullptr),
-	pVisionSearchAction_(nullptr), boneIndex_(-1), partIndex_(-1)
+ThrowEnemy::ThrowEnemy(GameObject* parent)
+	: EnemyBase(parent, "ThrowEnemy"), hModel_(-1), pMoveAction_(nullptr), pRotateAction_(nullptr), pVisionSearchAction_(nullptr),
+	boneIndex_(-1), partIndex_(-1) 
 {
 }
 
-Feet::~Feet()
+ThrowEnemy::~ThrowEnemy()
 {
 }
 
-void Feet::Initialize()
+void ThrowEnemy::Initialize()
 {
+	//モデルデータのロード
 	hModel_ = Model::Load("Model/stoneGolem.fbx");
 	assert(hModel_ >= 0);
 
@@ -32,6 +34,7 @@ void Feet::Initialize()
 	XMFLOAT3 startPos = pCreateStage->GetRandomFloarPosition();
 	transform_.position_ = startPos;
 	transform_.rotate_.y = (float)(rand() % 360);
+	transform_.scale_ = { 0.5f, 0.5f, 0.5f };
 
 	maxHp_ = 100;
 	hp_ = maxHp_;
@@ -40,16 +43,13 @@ void Feet::Initialize()
 	attackDamage_ = 50;
 
 	//Colliderの設定
-	SphereCollider* collision1 = new SphereCollider(XMFLOAT3(0, 0.5, 0), 0.75f);
-	SphereCollider* collision2 = new SphereCollider(XMFLOAT3(0, 1.5, 0), 0.75f);
-	pHandCollider_ = new SphereCollider(XMFLOAT3(0, 0, 0), 0.5f);
-	pHandCollider_->SetValid(false);
+	SphereCollider* collision1 = new SphereCollider(XMFLOAT3(0, 0.25, 0), 0.325f);
+	SphereCollider* collision2 = new SphereCollider(XMFLOAT3(0, 0.75, 0), 0.325f);
 	AddCollider(collision1);
 	AddCollider(collision2);
-	AddAttackCollider(pHandCollider_);
 
 	pEnemyUi_ = new EnemyUi(this);
-	pEnemyUi_->Initialize(2.5f);
+	pEnemyUi_->Initialize(1.25f);
 
 	//Actionの設定
 	pMoveAction_ = new AstarMoveAction(this, 0.0f, 0.3f);
@@ -57,44 +57,24 @@ void Feet::Initialize()
 	pVisionSearchAction_ = new VisionSearchAction(this, 30.0f, 90.0f);
 	pRotateAction_->Initialize();
 
-	//ステートの設定
-	pStateManager_ = new StateManager(this);
-	pStateManager_->AddState(new FeetAppear(pStateManager_));
-	pStateManager_->AddState(new FeetIdle(pStateManager_));
-	pStateManager_->AddState(new FeetPatrol(pStateManager_));
-	pStateManager_->AddState(new FeetCombat(pStateManager_));
-	pStateManager_->AddState(new FeetDead(pStateManager_));
-	pStateManager_->ChangeState("Appear");
-	pStateManager_->Initialize();
-	
-	//CombatStateの設定
-	pCombatStateManager_ = new StateManager(this);
-	pCombatStateManager_->AddState(new FeetWait(pCombatStateManager_));
-	pCombatStateManager_->AddState(new FeetMove(pCombatStateManager_));
-	pCombatStateManager_->AddState(new FeetAttack(pCombatStateManager_));
-	pCombatStateManager_->ChangeState("Wait");
-	pCombatStateManager_->Initialize();
-	
 	Model::GetBoneIndex(hModel_, "attack_Hand.R", &boneIndex_, &partIndex_);
 	assert(boneIndex_ >= 0);
-
 }
 
-void Feet::Update()
+void ThrowEnemy::Update()
 {
 	GameManager::GetCollisionMap()->CalcMapWall(transform_.position_, 0.3f);
 	pStateManager_->Update();
 
 }
 
-void Feet::Draw()
+void ThrowEnemy::Draw()
 {
 	pEnemyUi_->Draw();
 
 	XMFLOAT3 center = Model::GetBoneAnimPosition(hModel_, boneIndex_, partIndex_);
 	center = XMFLOAT3(center.x - transform_.position_.x, center.y - transform_.position_.y, center.z - transform_.position_.z);
-	pHandCollider_->SetCenter(center);
-
+	
 	Model::SetTransform(hModel_, transform_);
 	Model::Draw(hModel_);
 
@@ -103,7 +83,7 @@ void Feet::Draw()
 
 }
 
-void Feet::Release()
+void ThrowEnemy::Release()
 {
 	SAFE_DELETE(pVisionSearchAction_);
 	SAFE_DELETE(pRotateAction_);
@@ -114,17 +94,17 @@ void Feet::Release()
 
 }
 
-void Feet::ApplyDamage(int da)
+void ThrowEnemy::ApplyDamage(int da)
 {
 	EnemyBase::ApplyDamage(da);
 
-	if (pStateManager_->GetName() != "Combat") {
-		pStateManager_->ChangeState("Combat");
-	}
+	//if (pStateManager_->GetName() != "Combat") {
+	//	pStateManager_->ChangeState("Combat");
+	//}
 
 }
 
-void Feet::OnCollision(GameObject* pTarget)
+void ThrowEnemy::OnCollision(GameObject* pTarget)
 {
 	std::string name = pTarget->GetObjectName();
 	if (pTarget->GetObjectName().find("Enemy") != std::string::npos || name == "Player") {
--- Enemy/ThrowEnemy.h
+++ Enemy/ThrowEnemy.h
@@ -2,26 +2,24 @@
 #include "EnemyBase.h"
 #include "../State/StateBase.h"
 
-class SphereCollider;
 class AstarMoveAction;
 class RotateAction;
 class VisionSearchAction;
 
-class Feet : public EnemyBase
+class ThrowEnemy : public EnemyBase
 {
     int hModel_;
     int boneIndex_;
     int partIndex_;
-    SphereCollider* pHandCollider_;
-
+    
     //Action
     AstarMoveAction* pMoveAction_;
     RotateAction* pRotateAction_;
     VisionSearchAction* pVisionSearchAction_;
 
 public:
-    Feet(GameObject* parent);
-    ~Feet() override;
+    ThrowEnemy(GameObject* parent);
+    ~ThrowEnemy() override;
     void Initialize() override;
     void Update() override;
     void Draw() override;
@@ -29,7 +27,6 @@ public:
     void ApplyDamage(int da) override;
     void OnCollision(GameObject* pTarget) override;
 
-    SphereCollider* GetSphereCollider() { return pHandCollider_; }
     AstarMoveAction* GetMoveAction() { return pMoveAction_; }
     RotateAction* GetRotateAction() { return pRotateAction_; }
     VisionSearchAction* GetVisionSearchAction() { return pVisionSearchAction_; }

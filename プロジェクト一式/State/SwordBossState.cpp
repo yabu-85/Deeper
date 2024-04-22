e218cd50bfe420805a599107faecf7406c407646
海斗 矢吹 <220177@jc-21.jp>
2024-04-22 13:06:26 +0900

    ぼす作成

--- プロジェクト一式/State/SwordBossState.cpp
+++ プロジェクト一式/State/SwordBossState.cpp
@@ -31,12 +31,6 @@ namespace {
 	static const float SLOW_SPEED = 0.04f;
 	static const float ROTATE_RATIO = 0.07f;
 
-	//攻撃の情報
-	static const int ROTATE_FRAME = 45;
-	static const float ATTACK_READY_DISTANCE = 2.0f;
-
-	static const int CALC_FRAME[3][2] = { { 37, 80 }, { 40, 56 }, { 0, 100 } };
-
 }
 
 SwordBossAppear::SwordBossAppear(StateManager* owner) : StateBase(owner), time_(0)
@@ -270,6 +264,10 @@ struct AttackData {
 	pair<int, int> attack;		//攻撃判定フレーム
 	pair<int, int> rotateMove;	//移動・回転フレーム
 	
+	bool IsAttackFrame(int time) { return attack.first >= time && attack.second <= time; }
+	bool IsAttackEndFrame(int time) { return attack.second == time; }
+	bool IsRotateMoveFrame(int time) { return rotateMove.first >= time && rotateMove.second <= time; }
+
 	AttackData(int number, int anim, vector<int> derivat) : attackNumber(number), animId(anim), derivative(derivat) {};
 	AttackData(int number, int anim, vector<int> derivat, pair<int, int> atk, pair<int, int> rm)
 		: attackNumber(number), animId(anim), derivative(derivat), attack(atk), rotateMove(rm) {};
@@ -284,31 +282,21 @@ struct AttackManager {
 	bool IsNextReady() { return isNextReady; }
 	
 	bool IsInfoListEmpty() { return infoList.empty(); }
-	void AddList(const AttackData& info) {
-		list.push_back(info);
-	}
+	void AddList(const AttackData& info) { list.push_back(info); }
 
 	void SelectAction() {
 		infoList.clear();
 		std::vector<AttackData> copyList = list;
+		int numSelected = std::rand() % list.size() + 1;
 
-		/*
-		// データをシャッフルする
-		for (int i = 0; i < data.size(); ++i) {
-			int j = std::rand() % (i + 1);
-			std::swap(data[i], data[j]);
-		}
-
-	    // １つ以上データをランダムに選択する
-		int numSelected = std::rand() % data.size() + 1;
-		*/
-
-		//重複を避けてランダムに選択
+		//ランダム並べ替え
 		for (int i = 0; i < list.size(); ++i) {
-			int randomIndex = rand() % copyList.size();
-			infoList.push_back(copyList[randomIndex].attackNumber);	
-			copyList.erase(copyList.begin() + randomIndex);
+			int j = rand() % (i + 1);
+			std::swap(copyList[i], copyList[j]);
 		}
+
+		//選択する
+		for (int i = 0; i < list.size(); i++) infoList.push_back(copyList.at(i).attackNumber);
 	};
 
 	//今やってる攻撃終わった
@@ -324,11 +312,13 @@ struct AttackManager {
 
 };
 
-//あったくなんばーはManagerのほうで計算・取得したほうがよさげなきがすー
-
 namespace {
-	AttackData atk1 = AttackData(0, (int)SWORDBOSS_ANIMATION::ATTACK1, { Slash_Right }, { 37, 80 }, { 0, 45 });
-	AttackData atk2 = AttackData(1, (int)SWORDBOSS_ANIMATION::ATTACK2, { Slash_Up }, { 40, 56 }, { 0, 45 });
+	AttackData ATTACK_INFO_LIST[] = {
+		AttackData(0, (int)SWORDBOSS_ANIMATION::ATTACK1, { Slash_Right }, { 37, 80 }, { 0, 45 }),
+		AttackData(1, (int)SWORDBOSS_ANIMATION::ATTACK2, { Slash_Up }, { 40, 56 }, { 0, 45 }),
+		AttackData(1, (int)SWORDBOSS_ANIMATION::ATTACK2, { Slash_Up }, { 40, 56 }, { 0, 45 }),
+	};
+
 	AttackManager atk = AttackManager();
 
 }
@@ -337,8 +327,7 @@ SwordBossAttack::SwordBossAttack(StateManager* owner) : StateBase(owner), time_(
 {
 	pBoss_ = static_cast<SwordBoss*>(owner_->GetGameObject());
 
-	atk.AddList(atk1);
-	atk.AddList(atk2);
+	for (int i = 0; i < std::size(ATTACK_INFO_LIST) -1; i++) atk.AddList(ATTACK_INFO_LIST[i]);
 	atk.SelectAction();
 }
 
@@ -346,25 +335,30 @@ void SwordBossAttack::Update()
 {
 	time_++;
 	int number = atk.GetAttackNumber();
-
 	if (number < 0 || number >= 2) {
-		int num = number;
-
+		number = rand() % 2;
 	}
 
 	switch (number) {
-	case Slash_Up:
+	case (int)Slash_Up:
 		Attack1Update();
 		break;
-	case Slash_Right:
+	case (int)Slash_Down:
 		Attack2Update();
 		break;
 	default:
+		OutputDebugString("Attack No 1&2\n\n");
 		break;
 	}
 
-	if (time_ >= CALC_FRAME[number][0] && time_ <= CALC_FRAME[number][1]) { pBoss_->CalcPoly(); }
-	if (time_ == CALC_FRAME[number][1]) { pBoss_->AttackEnd(); }
+	//回転・移動
+	if (atk.list[number].IsRotateMoveFrame(time_)) {
+		pBoss_->GetRotateAction()->Update();
+		pBoss_->GetOrientedMoveAction()->SetTarget(GameManager::GetPlayer()->GetPosition());
+		pBoss_->GetOrientedMoveAction()->Update();
+	}
+	if (atk.list[number].IsAttackFrame(time_)) pBoss_->CalcPoly();
+	if (atk.list[number].IsAttackEndFrame(time_)) pBoss_->AttackEnd();
 
 	int startT = pBoss_->GetAnimationController()->GetAnim(atk.list[number].animId).startFrame;
 	int endT = pBoss_->GetAnimationController()->GetAnim(atk.list[number].animId).endFrame;
@@ -385,8 +379,9 @@ void SwordBossAttack::OnEnter()
 	time_ = 0;
 	pBoss_->GetOrientedMoveAction()->SetDirection(XMVECTOR{ 0, 0, 1, 0 });
 	pBoss_->SetCombatReady(false);
-
 	pBoss_->GetAnimationController()->SetNextAnime(atk.list[atk.GetAttackNumber()].animId, 1.0f);
+
+	atk.SetIsNextReady(false);
 }
 
 void SwordBossAttack::OnExit()
@@ -401,13 +396,9 @@ void SwordBossAttack::Attack1Update()
 {
 	OutputDebugString("Attack1\n\n");
 	
-	//回転・移動
-	if (time_ < ROTATE_FRAME) {
-		pBoss_->GetRotateAction()->Update();
-		pBoss_->GetOrientedMoveAction()->SetTarget(GameManager::GetPlayer()->GetPosition());
-		pBoss_->GetOrientedMoveAction()->Update();
-	}
-
+	float dist = CalculationDistance(GameManager::GetPlayer()->GetPosition(), pBoss_->GetPosition());
+	if (dist <= 3.0f) atk.SetIsNextReady(true);
+	else atk.SetIsNextReady(false);
 
 }
 
@@ -415,12 +406,9 @@ void SwordBossAttack::Attack2Update()
 {
 	OutputDebugString("Attack2\n\n");
 	
-	//回転・移動
-	if (time_ < ROTATE_FRAME) {
-		pBoss_->GetRotateAction()->Update();
-		pBoss_->GetOrientedMoveAction()->SetTarget(GameManager::GetPlayer()->GetPosition());
-		pBoss_->GetOrientedMoveAction()->Update();
-	}
+	float dist = CalculationDistance(GameManager::GetPlayer()->GetPosition(), pBoss_->GetPosition());
+	if (dist <= 3.0f) atk.SetIsNextReady(true);
+	else atk.SetIsNextReady(false);
 
 }
 
